d3ff916ae704760260470392ec51435a17831351
lilharry <hlum1@stuy.edu>
2016-02-22 13:20:29 -0500

    created bronze.java

---
+++ 03knight/KnightBoard.java
@@ -0,0 +1,20 @@
+public class KnightBoard{
+    private int[][] board;
+    private int size;
+
+    public KnightBoard(int size){
+	board = new int[size][size];
+	this.size = size;
+    }
+
+    public boolean solve(){
+
+    }
+
+    public boolean solve(){
+
+    }
+
+    public void printSolution(){
+
+    }
---
+++ 04USACO/Bronze.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Bronze{
+    int R,C,E,N;
+    int[][] map;
+    int[] R_s;
+    int[] C_s;
+    int[] D_s;
+    public Bronze(String input){
+	Scanner input = new Scanner(new File(input));
+	while (fileScanner.hasNextInt()){    
+	}
+    }
+
+    public static void main(String[] args){
+	System.out.println(Bronze(args[0]));
+    }
+}
