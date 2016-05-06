f4fb7bd24a2a4ba334da82806d53a21b4076dfb2
lilharry <hlum1@stuy.edu>
2016-05-06 13:15:03 -0400

    a

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -12,20 +12,50 @@ public class MyHeap<T extends Comparable<T>>
    
    public MyHeap(T[] array){
        size = array.length;
-       data = new Comparable[size]; 
+       data = new Comparable[size];
+       heapify(data);
    }
+   
    private void pushDown(int k){
-       
+       if (k <= size / 2){ //works only if k has children
+	   T temp = data[k];
+	   data[k] = data[k*2];
+	   data[k*2] = temp;
+       }
+
    }
-   private void pushUp(int k)
-   private void heapify()
-   public T delete()
-   public void add(T x)
-   private void doubleSize()
-   public String toString()
+
+   private void pushUp(int k){
+       T temp = data[k];
+       data[k] = data[k/2];
+       data[k/2] = temp;
+   }
+   
+   private void heapify(){}
+
+   public T delete(){
+
+   }
+
+   public void add(T x){
+       if (size == 0){
+       }
+   }
+
+   private void grow(){
+       int temp = new Comparable[data.length*2];
+       for (int i = 0; i < data.length; i++){
+	   temp[i] = data[i];
+       }
+       data = temp;
+   }
+
+       
+   private void doubleSize(){}
+   public String toString(){}
 
    //do this last
-   public MyHeap(boolean isMax)
-   public MyHeap(T[] array, boolean isMax)
+   public MyHeap(boolean isMax){}
+   public MyHeap(T[] array, boolean isMax){}
 
 }
